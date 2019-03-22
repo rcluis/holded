bd0d7d9a85440616f44303d1fa157303e3e02679
raul.catalan <209029461r>
2019-03-22 14:41:06 +0100

    add user

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # holded
 
 - I used different collections for companies and users because of the limit size of 4mb for each collection, so I case we have a lot of users inside a company this would be a limitation.
+- add i18n
+- split in components
+- add profile image limit size
--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from "axios";
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { FaTrash, FaUserEdit } from "react-icons/fa"
+import './App.scss';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
-import './App.css';
 
 class App extends Component {
 	state = {
 		users: [],
-		companyId: '5c90f3971c9d4400002b3703'
+		companyId: '5c90f3971c9d4400002b3703',
+		showUserModal: false,
+		newUser: {
+			name: 'afasdf',
+			surname: '',
+			email: '',
+			position: '',
+			office: '',
+			salary: 0,
+			workingHours: 0,
+			profilePictureFile: null
+		}
 	};
 
 	componentDidMount() {
@@ -37,6 +48,25 @@ class App extends Component {
 		});
 	};
 
+	addUser = () => {
+		console.log(this.state.newUser);
+		var FR= new FileReader();
+
+		FR.addEventListener("load", function(e) {
+			console.log(e.target.result);
+		});
+
+		FR.readAsDataURL(this.state.newUser.profilePictureFile);
+	};
+
+	toggleUserModal = () => {
+		this.setState({showUserModal: !this.state.showUserModal});
+	};
+
+	handleProfileImage = (e) => {
+		this.setState({ newUser: { ...this.state.newUser, profilePictureFile: e.target.files[0] } });
+	};
+
 	render() {
 		const { users } = this.state;
 
@@ -46,7 +76,7 @@ class App extends Component {
 				text: '',
 				formatter: (cell) => {
 					var base64data = new Buffer(cell.data).toString('base64');
-					return <img src={"data:image/png;base64, " + base64data} width={30} alt="profile"/>;
+					return <img src={"data:image/png;base64, " + base64data} width={30} alt="profile" />;
 				}
 			},
 			{ dataField: 'name', text: 'Name', sort: true },
@@ -65,7 +95,7 @@ class App extends Component {
 				events: {
 					onClick: (e, column, columnIndex, row, rowIndex) => {
 						console.log(row);
-						this.setState({'users': []});
+						this.setState({users: []});
 					}
 				}
 			},
@@ -77,7 +107,6 @@ class App extends Component {
 				),
 				events: {
 					onClick: (e, column, columnIndex, row, rowIndex) => {
-						console.log(row._id);
 						const userId = row._id;
 						this.deleteUser(userId, rowIndex);
 					}
@@ -91,8 +120,99 @@ class App extends Component {
 			// hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
 			showTotal: true,
 		};
+
 		return (
 			<div className="container">
+				<Button variant="primary" onClick={this.toggleUserModal}>Add user</Button>
+
+				<Modal show={this.state.showUserModal} onHide={this.toggleUserModal}>
+					<Modal.Header closeButton>
+						<Modal.Title>Modal heading</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form>
+							<Form.Group	controlId="name">
+								<Form.Label>Name</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter name"
+									value={this.state.newUser.name}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, name: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="surname">
+								<Form.Label>Surname</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter surname"
+									value={this.state.newUser.surname}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, surname: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="email">
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="Enter email"
+									value={this.state.newUser.email}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, email: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="position">
+								<Form.Label>Position</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter position"
+									value={this.state.newUser.position}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, position: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="office">
+								<Form.Label>Office</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter office"
+									value={this.state.newUser.office}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, office: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="salary">
+								<Form.Label>Salary</Form.Label>
+								<Form.Control
+									type="number"
+									placeholder="Enter salary"
+									value={this.state.newUser.office}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, salary: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="workingHours">
+								<Form.Label>Working hours</Form.Label>
+								<Form.Control
+									type="number"
+									placeholder="Enter working hours"
+									value={this.state.newUser.workingHours}
+									onChange={e => this.setState({ newUser: { ...this.state.newUser, workingHours: e.target.value} })}
+								/>
+							</Form.Group>
+							<Form.Group controlId="profileImage">
+								<Form.Label>Profile image</Form.Label>
+								<Form.Control
+									type="file"
+									onChange={this.handleProfileImage}
+									accept=".png, .jpg"/>
+							</Form.Group>
+						</Form>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={this.toggleUserModal}>
+							Close
+						</Button>
+						<Button variant="primary" onClick={this.addUser}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+
 				<BootstrapTable bootstrap4
 								bordered={ false }
 								hover={ true }
